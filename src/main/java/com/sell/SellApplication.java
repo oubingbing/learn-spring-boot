04d406feffb39d298a8b9f8b6b821492d8736264
ouzhibing <ouzhibing@galaxymx.com>
2018-08-03 18:13:21 +0800

    mybatis

--- src/main/java/com/sell/SellApplication.java
+++ src/main/java/com/sell/SellApplication.java
@@ -1,11 +1,13 @@
 package com.sell;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.sell.*"})
 @SpringBootApplication
+@MapperScan(basePackages = "com.sell.dataobject.mapper")
 public class SellApplication {
 
 	public static void main(String[] args) {
